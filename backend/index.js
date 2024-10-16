85d9f24a85ed54f40c5bc9b0f68d762db89f2e6d
danishshaikh04 <danishshaikh0595@gmail.com>
2024-10-16 12:55:09 +0530

    production ready code

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -7,10 +7,14 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 
 const app = express();
+
+
+const _dirname = path.resolve();
 
 // middleware
 app.use(express.json());
@@ -32,6 +36,11 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.get('*', (req,res)=>{
+    response.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+});
 
 
 app.listen(PORT,()=>{
